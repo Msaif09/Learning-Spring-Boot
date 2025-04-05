9f486a32f12fbc5bf1f1f641fca2d7db94e9d117
Mohammad Saif <msaif2347@gmail.com>
2025-04-05 14:32:04 +0530

    Learn to create API's

--- first/src/main/java/com/learn/test/first/controllers/EmployeeController.java
+++ first/src/main/java/com/learn/test/first/controllers/EmployeeController.java
@@ -7,15 +7,28 @@ package com.learn.test.first.controllers;
 
 
 import com.learn.test.first.dto.EmployeeDTO;
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping(path = "/employees")
-    public EmployeeDTO getEmployee(){
-        return new EmployeeDTO(9L,"Mohammad Saif", LocalDate.of(2023,7,15),true);
+
+    //mandatory to pass id
+    @GetMapping(path = "/employees/{id}")
+    public EmployeeDTO getEmployee(@PathVariable("id") Long id){
+        return new EmployeeDTO(id,"Mohammad Saif", LocalDate.now(),true);
+    }
+
+
+    //optional to pass sortBy
+    //you can pass multiple params
+    @GetMapping(path = "/employee")
+    public String getData(@PathParam("sortBy") String sortBy ,
+                          @PathParam("limit") Integer limit){
+        return "Hello " + sortBy + " " + limit;
     }
 }
--- first/src/main/java/com/learn/test/first/dto/EmployeeDTO.java
+++ first/src/main/java/com/learn/test/first/dto/EmployeeDTO.java
@@ -1,17 +1,55 @@
 package com.learn.test.first.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class EmployeeDTO {
 
+
+public class EmployeeDTO {
     private Long id;
     private String name;
     private LocalDate dateOfJoining;
-    private boolean isActive;
+    private boolean active;
+
+    public EmployeeDTO(Long id, String name, LocalDate dateOfJoining, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.dateOfJoining = dateOfJoining;
+        this.active = active;
+    }
+
+    public EmployeeDTO() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return this.dateOfJoining;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
 }
